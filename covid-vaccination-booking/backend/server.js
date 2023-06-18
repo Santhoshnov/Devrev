645abd5ab9b2c3628aaf8192b4e9b70e2209d183
Santhosh S <108118100+Santhoshnov@users.noreply.github.com>
2023-06-18 12:28:35 +0530

    fullstack application

---
+++ covid-vaccination-booking/backend/server.js
@@ -0,0 +1,442 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: true,
+  saveUninitialized: true
+}));
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb+srv://santhosh13112002:MrtNyDKGsTUS8VC5@cluster0.jikeuia.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  password: String,
+});
+
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+
+
+const centreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  workingHours: {
+    type: String,
+    required: true,
+  },
+
+  slotsAvailable: {
+    type: Number,
+    default: 10,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+const Admin = mongoose.model('Admin', adminSchema);
+const Centre = mongoose.model('Centre', centreSchema);
+
+app.get('/centers', async (req, res) => {
+  try {
+    const { search } = req.query;
+    const query = {
+      $or: [
+        { name: { $regex: search, $options: 'i' } },
+        { address: { $regex: search, $options: 'i' } },
+      ],
+    };
+    const centers = await Centre.find(query);
+
+    const formattedCenters = centers.map((center) => {
+      const { name, address, workingHours, slotsAvailable } = center;
+
+      return {
+        name,
+        address,
+        workingHours,
+        slotsAvailable,
+      };
+    });
+
+    res.json({ centers: formattedCenters });
+  } catch (error) {
+    console.error('Error searching for vaccination centers:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
+app.post('/slots', async (req, res) => {
+  try {
+    const { name, age, centerName, centerAddress } = req.body;
+
+    const centre = await Centre.findOne({ name: centerName, address: centerAddress });
+
+    if (!centre) {
+      return res.status(404).json({ error: 'Vaccination center not found' });
+    }
+
+    if (centre.slotsAvailable === 0) {
+      return res.status(400).json({ error: 'No slots available' });
+    }
+
+    const maxSlotsPerDay = 10;
+    const currentDate = new Date().setHours(0, 0, 0, 0);
+    const slotsBookedToday = await Centre.countDocuments({
+      name: centerName,
+      address: centerAddress,
+      bookedDate: { $gte: currentDate },
+    });
+
+    if (slotsBookedToday >= maxSlotsPerDay) {
+      return res.status(400).json({ error: 'Maximum slots reached for today' });
+    }
+
+    centre.slotsAvailable--;
+    centre.bookedDate = new Date();
+    await centre.save();
+
+    console.log('Vaccination slot booked successfully');
+    res.json({ message: 'Vaccination slot booked successfully' });
+  } catch (error) {
+    console.error('Error booking vaccination slot:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const setupAdmin = async () => {
+  try {
+    const admin = await Admin.findOne({ username: 'admin' });
+    if (!admin) {
+      const hashedPassword = await bcrypt.hash('shiro@sandy', 10);
+      await Admin.create({ username: 'admin', password: hashedPassword });
+      console.log('Admin account created successfully.');
+    } else {
+      console.log('Admin account already exists.');
+    }
+  } catch (error) {
+    console.error('Error during admin setup:', error);
+  }
+};
+
+setupAdmin();
+
+
+
+
+
+app.post('/admin-login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.redirect('/admin-login?error=InvalidCredentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, admin.password);
+    if (!isPasswordValid) {
+      return res.redirect('/admin-login?error=InvalidCredentials');
+    }
+
+    req.session.isAdmin = true;
+    return res.redirect('/admin/dashboard');
+  } catch (error) {
+    console.error('Error during admin login:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+const isAdmin = (req, res, next) => {
+  if (req.session.isAdmin) {
+    next();
+  } else {
+    res.redirect('/admin-login');
+  }
+};
+
+app.post('/admin/addCentre', isAdmin, async (req, res) => {
+  const { name, address, workingHours,} = req.body;
+  try {
+    const newCentre = new Centre({ name, address, workingHours,slotsAvailable: 10 });
+    await newCentre.save();
+    console.log('Vaccination centre added successfully!');
+    res.send('Vaccination centre added successfully!');
+  } catch (error) {
+    console.error('Error adding vaccination centre:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+app.post('/admin/removeCentre', isAdmin, async (req, res) => {
+  const { name, address } = req.body;
+  console.log('Name:', name);
+  console.log('Address:', address);
+
+  try {
+    const deletedResult = await Centre.deleteOne({ name, address });
+
+    if (deletedResult.deletedCount === 1) {
+      console.log('Vaccination centre removed successfully!');
+      return res.json({ message: 'Vaccination centre removed successfully!' });
+    } else {
+      console.error('Error removing vaccination centre');
+      return res.status(500).json({ error: 'Failed to remove vaccination centre. Please try again later.' });
+    }
+  } catch (error) {
+    console.error('Error removing vaccination centre:', error);
+    return res.status(500).json({ error: 'Failed to remove vaccination centre. Please try again later.' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/admin/dosageDetails', isAdmin, async (req, res) => {
+  try {
+    const dosageDetails = await Centre.aggregate([
+      {
+        $group: {
+          _id: {
+            name: '$name',
+            address: '$address',
+            workingHours: '$workingHours',
+          },
+          slotsAvailable: { $sum: '$slotsAvailable' },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          name: '$_id.name',
+          address: '$_id.address',
+          workingHours: '$_id.workingHours',
+          slotsAvailable: 1,
+        },
+      },
+    ]);
+
+    console.log('Fetched dosage details successfully');
+    res.json({ dosageDetails });
+  } catch (error) {
+    console.error('Error retrieving dosage details:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/admin/dashboard', isAdmin, (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'admin_dashboard.html'));
+});
+
+app.get('/admin/logout', (req, res) => {
+  req.session.isAdmin = false;
+  req.session.adminId = null; 
+  res.redirect('/admin-login');
+});
+
+
+app.get('/admin-login', (req, res) => {
+  if (req.session.isAdmin) {
+    return res.redirect('/admin/dashboard');
+  }
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'admin_login.html'));
+});
+
+
+
+
+
+
+
+app.post('/api/signup', [
+  check('email', 'Valid email is required').isEmail(),
+  check('password', 'Password must be at least 8 characters and contain alphabets, special symbols, and numbers').matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/),
+  check('phone', 'Phone number must be 10 digits').isLength({ min: 10, max: 10 })
+], async (req, res) => {
+  const { email, password, phone } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: 'User with this email already exists' });
+    }
+
+    const newUser = new User({
+      email,
+      password: await bcrypt.hash(password, 10),
+      phone
+    });
+    await newUser.save();
+
+    req.session.userId = newUser._id;
+
+    return res.json({ message: 'Signup successful' });
+  } catch (error) {
+    console.error('Error during signup:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/api/login', [
+  check('email', 'Valid email is required').isEmail(),
+  check('password', 'Password is required').notEmpty()
+], async (req, res) => {
+  const { email, password } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401)    .json({ error: 'Invalid password' });
+    }
+
+    req.session.userId = user._id;
+
+    return res.json({ message: 'Login successful' });
+  } catch (error) {
+    console.error('Error during login:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/dashboard', (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect('/login');
+  }
+
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dashboard.html'));
+});
+
+
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
+app.get('/admin/script.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'script.js'));
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'login.html'));
+});
+
+
+app.get('/login', (req, res) => {
+  if (req.session.userId) {
+    return res.redirect('/dashboard');
+  }
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'login.html'));
+});
+
+app.get('/signup', (req, res) => {
+  if (req.session.userId) {
+    return res.redirect('/dashboard');
+  }
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'signup.html'));
+});
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
---
+++ covid-vaccination-booking/frontend/script.js
@@ -0,0 +1,297 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('login-form');
+  const signupForm = document.getElementById('signup-form');
+  const adminLoginForm = document.getElementById('admin-login-form');
+
+  if (loginForm) {
+    loginForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+
+      const response = await fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert('Login successful');
+        window.location.href = '/dashboard';
+      } else {
+        alert('Invalid email or password');
+        const errors = data.errors.map((error) => `<li>${error.msg}</li>`);
+        const loginErrors = document.getElementById('login-errors');
+        if (loginErrors) {
+          loginErrors.innerHTML = errors.join('');
+        }
+      }
+    });
+  }
+
+  if (signupForm) {
+    signupForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      
+      const email = document.getElementById('signup-email').value;
+      const password = document.getElementById('signup-password').value;
+      const phone = document.getElementById('signup-phone').value;
+
+      const response = await fetch('http://localhost:3000/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password, phone }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert('Signup successful');
+        window.location.href = '/login.html';
+      } else {
+        alert('Failed to signup');
+        const errors = data.errors.map((error) => `<li>${error.msg}</li>`);
+        const signupErrors = document.getElementById('signup-errors');
+        if (signupErrors) {
+          signupErrors.innerHTML = errors.join('');
+        }
+      }
+    });
+  }
+
+  if (adminLoginForm) {
+    adminLoginForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+
+      const response = await fetch('http://localhost:3000/admin-login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        window.location.href = '/admin/dashboard';
+      } else {
+        alert('Invalid username or password');
+      }
+    });
+  }
+
+  const addCentreForm = document.getElementById('add-centre-form');
+
+  if (addCentreForm) {
+    addCentreForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+  
+      const name = document.getElementById('centre-name').value;
+      const address = document.getElementById('centre-address').value;
+      const workingHours = document.getElementById('centre-working-hours').value;
+  
+      try {
+        const response = await fetch('http://localhost:3000/admin/addCentre', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, address, workingHours}),
+        });
+  
+        if (response.ok) {
+          alert('Vaccination centre added successfully!');
+        } else {
+          throw new Error('Failed to add vaccination centre');
+        }
+      } catch (error) {
+        console.error('Error adding vaccination centre:', error);
+        alert('Failed to add vaccination centre. Please try again later.');
+      }
+    });
+  }
+  
+
+
+  
+  const removeCentreForm = document.getElementById('remove-centre-form');
+
+  if (removeCentreForm) {
+    removeCentreForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+  
+      const nameInput = document.getElementById('remove-centre-name');
+      const addressInput = document.getElementById('remove-centre-address');
+  
+      const name = nameInput.value;
+      const address = addressInput.value;
+  
+      console.log('Name:', name);
+      console.log('Address:', address);
+  
+      try {
+        const response = await fetch('http://localhost:3000/admin/removeCentre', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, address }),
+        });
+  
+        if (response.ok) {
+          alert('Vaccination centre removed successfully!');
+          $('#removeCentreModal').modal('hide');
+        } else if (response.status === 404) {
+          throw new Error('Vaccination centre not found');
+        } else {
+          throw new Error('Failed to remove vaccination centre');
+        }
+      } catch (error) {
+        console.error('Error removing vaccination centre:', error);
+        alert('Failed to remove vaccination centre. Please try again later.');
+      }
+    });
+  }
+  
+  
+  
+
+  const getDosageDetailsLink = document.getElementById('get-dosage-details-link');
+  const dosageDetailsContainer = document.getElementById('dosage-details-container');
+  
+  async function fetchDosageDetails() {
+    try {
+      const response = await fetch('http://localhost:3000/admin/dosageDetails');
+      const data = await response.json();
+  
+      if (response.ok) {
+        const dosageDetails = data.dosageDetails;
+        displayDosageDetails(dosageDetails);
+      } else {
+        throw new Error(data.error || 'Failed to fetch dosage details');
+      }
+    } catch (error) {
+      console.error('Error fetching dosage details:', error);
+      alert('Failed to fetch dosage details. Please try again later.');
+    }
+  }
+  
+  if (getDosageDetailsLink) {
+    getDosageDetailsLink.addEventListener('click', fetchDosageDetails);
+  }
+  
+  function displayDosageDetails(dosageDetails) {
+    dosageDetailsContainer.innerHTML = '';
+  
+    if (dosageDetails.length > 0) {
+      dosageDetails.forEach((centre) => {
+        const { name, address, workingHours, slotsAvailable } = centre;
+  
+        const centreElement = document.createElement('div');
+        centreElement.innerHTML = `
+          <p>Centre: ${name}</p>
+          <p>Address: ${address}</p>
+          <p>Working Hours: ${workingHours}</p>
+          <p>Available Slots: ${slotsAvailable}</p>
+          <hr>
+        `;
+  
+        dosageDetailsContainer.appendChild(centreElement);
+      });
+    } else {
+      dosageDetailsContainer.innerHTML = '<p>No dosage details available</p>';
+    }
+  }
+  
+  
+
+  const searchForm = document.getElementById('search-form');
+  const searchInput = document.getElementById('search-input');
+  const centresContainer = document.getElementById('centres-container');
+  
+  if (searchForm) {
+    searchForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+  
+      const searchValue = searchInput.value.trim();
+      if (searchValue === '') {
+        return;
+      }
+  
+      try {
+        const response = await fetch(`/centers?search=${encodeURIComponent(searchValue)}`);
+        const data = await response.json();
+  
+        if (response.ok) {
+          const centres = data.centers;
+          displayCentres(centres);
+        } else {
+          throw new Error(data.error || 'Failed to fetch vaccination centers');
+        }
+      } catch (error) {
+        console.error('Error searching for vaccination centers:', error);
+        alert('Failed to search for vaccination centers. Please try again later.');
+      }
+    });
+  }
+  
+  function displayCentres(centres) {
+    centresContainer.innerHTML = '';
+  
+    if (centres.length > 0) {
+      centres.forEach((centre) => {
+        const { _id, name, address, workingHours, slotsAvailable } = centre;
+  
+        const centreElement = document.createElement('div');
+        centreElement.innerHTML = `
+          <p>Name: ${name}</p>
+          <p>Address: ${address}</p>
+          <p>Working Hours: ${workingHours}</p>
+          <p>Available Slots: ${slotsAvailable}</p>
+          <button class="btn btn-primary" onclick="bookSlot('${_id}')">Book Slot</button>
+          <hr>
+        `;
+  
+        centresContainer.appendChild(centreElement);
+      });
+    } else {
+      centresContainer.innerHTML = '<p>No vaccination centers found</p>';
+    }
+  }
+  
+  
+  async function bookslot(name, age, centerName, centerAddress) {
+    try {
+      const response = await fetch('/slots', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, age, centerName, centerAddress }), 
+      });
+      const data = await response.json();
+  
+      if (response.ok) {
+        alert(data.message);
+        if (searchForm) {
+          searchForm.dispatchEvent(new Event('submit'));
+        }
+      } else {
+        throw new Error(data.error || 'Failed to book vaccination slot');
+      }
+    } catch (error) {
+      console.error('Error booking vaccination slot:', error);
+      alert('Failed to book vaccination slot. Please try again later.');
+    }
+  }
+  
+});
